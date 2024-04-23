5c5a9ffb6911f372a510bd420ef33454aa04c5b0
Vinay08082 <149697387+Vinay08082@users.noreply.github.com>
2024-04-23 19:48:09 +0530

    Add files via upload

---
+++ indexc.js
@@ -0,0 +1,15 @@
+setInterval(() =>{
+    d=new Date();
+    htime=d.getHours();
+    mtime=d.getMinutes();
+    stime=d.getSeconds();
+
+    hr=30*htime + mtime/2;
+    mr=6*mtime;
+    sr=6*stime;
+
+    hour.style.transform= `rotate(${hr}deg)`;
+    minute.style.transform= `rotate(${mr}deg)`;
+    seconds.style.transform= `rotate(${sr}deg)`;
+
+},1000);
